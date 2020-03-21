ebd9444b17ac16ce1a86cc9e2f5288d9df78eda7
Nati <natimirauta@gmail.com>
2020-03-21 16:39:32 +0200

    Created script that cleans Jenkins

---
+++ Auto_connect_Jenkins_nodes/5_cleanup_after_Docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NODE_NAME=$1
+JENKINS_URL=$2
+
+echo "[  OK  ] Disconnecting Jenkins node ${NODE_NAME}"
+
+java -jar /var/lib/jenkins/jenkins-cli.jar -s $JENKINS_URL disconnect-node $NODE_NAME
+
+echo "[  OK  ] Deleting ${NODE_NAME} "
+
+java -jar /var/lib/jenkins/jenkins-cli.jar -s $JENKINS_URL delete-node $NODE_NAME
+
